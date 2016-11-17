84b45532a2e0982e5c8bbbfa2e58642291f5e2e3
Carly N. Sullivan <carlysullivan2@gmail.com>
2016-11-17 18:53:25 -0500

    defining variables for ajax request

--- Assets/javascript/weatherLogic.js
+++ Assets/javascript/weatherLogic.js
@@ -1,4 +1,5 @@
 //possibly add class or id later to differentiate tops, bottoms, accessories.
+//following code from NYTSearch activity 6.3... not yet edited for weatherclothingwizard
 
 //start dumping to HTML
 var resultSection = $("<div>");
@@ -13,3 +14,46 @@ $("#resultSection-" + i).append("<img src='url.from.data'>" + ClothingObject.dat
 
 //empty div before adding html content
 $("#largeResultsDiv").empty();
+
+//-----------------above this line has not fully been changed yet for weatherclothingwizard
+
+//following example is from Giftastic homework, edited for weatherclothingwizard
+//id of navbar submit button?
+
+	$(".btn").on("click", function() {
+			//grabbing zip code from user input field
+		 	var zipcode = $(".form-control").val();
+		 	//console.log the zipcode var data
+		 	console.log(zipcode);
+
+			
+			var apiKey = "3f8d0b47644220a7"
+
+			var weatherRequest = ["conditions", "forecast"];
+
+			//Giphy API url plus specific queries requested, including dessert name from array
+			var currentURL = "https://api.wunderground.com/api/" + apiKey + "/" + weatherRequest[0] + "q/" + zipcode + ".json";
+
+			var forecastURL = "https://api.wunderground.com/api/" + apiKey + "/" + weatherRequest[1] + "q/" + zipcode + ".json";
+			//created variable for API data request to be made
+			var currentRequest = {
+				//url for reqest will === above var of urlQuery
+				url: currentURL,
+				//request method === GET
+				method: "GET"
+			};
+
+			var forecastRequest = {
+				//url for reqest will === above var of urlQuery
+				url: forecastURL,
+				//request method === GET
+				method: "GET"
+			};
+
+//---------------------above this line has been edited specifically for weatherclothingwizard
+//---------------------below this line is leftover from homework example
+
+			//request data from the Giphy API
+			$.ajax(request).done(function(response) {
+				//console.log data response, objects from API
+				console.log(response);
