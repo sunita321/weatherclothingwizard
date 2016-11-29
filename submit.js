edae74cd7ab444132cbbb5e488bf041690da933b
Luis Martinez <One2@Luiss-MacBook-Pro.local>
2016-11-28 20:46:15 -0500

    added sunitas navbar inputs

--- submit.js
+++ submit.js
@@ -2,8 +2,7 @@ $(document).ready(function()
 {
 	$("#wizard-search").on("click", function()
 	{
-		var gender = $('#gender-input').val();
-		console.log(gender);
+		
 
 		var occasion = $('#occasion-input').val();
 		console.log(occasion);
